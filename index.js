f87449df3d71d93bbf1d28010fe29dde1153b450
yavrum13 <yuval.avrum@gmail.com>
2024-02-01 15:34:23 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+let coolName = 'yuvie blue';
+
 const testVar = "hi"
 
-/* Enter the code to remove the node element here */
-document.querySelector("main#main").remove();
+//Test #1
+const mainElement = document.querySelector("main#main")
+// can also add .remove(); to the very end of the previous line,
+// but that would make more sense IF it wasn't assigned to the variable.
+mainElement.remove();
 
-/* Create your new element here and assign it to newHeader */
+//Test #2
 const newHeader = document.createElement("h1");
+
+//Test #3
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
+
+//Test #4
+switch (coolName){
+    case 'yuvie blue':
+        newHeader.innerHTML = `${coolName} is the champion!`;
+    default:
+        newHeader.innerHTML = "dang... nobody is the champion!";
+}
+//newHeader.innerHTML = "Yuvie is the champion!";
+
+//Test #5
 newHeader.className = "victory";
