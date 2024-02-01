78cad92314e6be10ad9097b023102e483d749d35
yavrum13 <yuval.avrum@gmail.com>
2024-02-01 15:40:45 -0600

    Corrected Branch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here!
+let coolName = 'yuvie blue';
+
+const testVar = "hi"
+
+//Test #1
+const mainElement = document.querySelector("main#main")
+// can also add .remove(); to the very end of the previous line,
+// but that would make more sense IF it wasn't assigned to the variable.
+mainElement.remove();
+
+//Test #2
+const newHeader = document.createElement("h1");
+
+//Test #3
+newHeader.id = "victory"
+
+//Test #4
+switch (coolName){
+    case 'yuvie blue':
+        newHeader.innerHTML = `${coolName} is the champion!`;
+    default:
+        newHeader.innerHTML = "dang... nobody is the champion!";
+}
+//newHeader.innerHTML = "Yuvie is the champion!";
+
+//Test #5
+newHeader.className = "victory";
